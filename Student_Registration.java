bb2595e99dd28d6598fd88925383c3ceaa8eca41
22P31A0594 <156417502+22P31A0594@users.noreply.github.com>
2024-01-21 13:01:18 +0530

    import java.util.*; public class Exam { public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int t=sc.nextInt();      while(t-->0)      {     int n=sc.nextInt();     int m=sc.nextInt();     int k=sc.nextInt();     if(n<=(m-k))     System.out.println("YES");     else     System.out.println("NO"); } } }

---
+++ Student_Registration.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Exam
+{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+     while(t-->0)
+     {
+    int n=sc.nextInt();
+    int m=sc.nextInt();
+    int k=sc.nextInt();
+    if(n<=(m-k))
+    System.out.println("YES");
+    else
+    System.out.println("NO");
+}
+}
+}
