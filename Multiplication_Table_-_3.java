fa6925303cb43985edaf5bf1927889c6463a9c73
22P31A0594 <156417502+22P31A0594@users.noreply.github.com>
2024-01-31 09:54:47 +0530

    import java.util.Scanner; public class Exam{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=a;i<=b;i++)         {             System.out.printf("%d x %d = %d ",n,i,n*i);         }     } }

---
+++ Multiplication_Table_-_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exam{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            System.out.printf("%d x %d = %d
+",n,i,n*i);
+        }
+    }
+}
