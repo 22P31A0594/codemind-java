e21c9628469d44dd2251bc54c573b519a785b15e
22P31A0594 <156417502+22P31A0594@users.noreply.github.com>
2024-02-12 19:18:59 +0530

    import java.util.Scanner; public class Exam{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String copy;         copy=s;         System.out.println(copy);     } }

---
+++ String_Copy.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Exam{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String copy;
+        copy=s;
+        System.out.println(copy);
+    }
+}
