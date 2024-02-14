a4898ac5313749a25496982141d25297eed1e89d
22P31A0594 <156417502+22P31A0594@users.noreply.github.com>
2024-02-14 06:27:54 +0530

    import java.util.Scanner; public class Exam{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int sum=0;         for(int i=0;i<s.length();i++){             char ch=s.charAt(i);             if(Character.isDigit(ch)){                 sum=sum+Character.getNumericValue(ch);             }         }         System.out.println(sum);     } }

---
+++ _Sum_of_all_numbers_present_in_the_String.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Exam{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)){
+                sum=sum+Character.getNumericValue(ch);
+            }
+        }
+        System.out.println(sum);
+    }
+}
