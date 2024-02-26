60a7706ead41dbc18fbab2c98a8de21632ec757e
Anusha <156417502+22P31A0594@users.noreply.github.com>
2024-02-26 20:33:57 +0530

    import java.util.Scanner; import java.util.Arrays; public class Exam{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++){          a[i]=a[i]*a[i];         }          Arrays.sort(a);         for(int i=0;i<n;i++){             System.out.print(a[i]+" ");         }     } }

---
+++ _Squares_of_a_Sorted_Array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Exam{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+         a[i]=a[i]*a[i];
+        }
+         Arrays.sort(a);
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
